a209abebc22afc1ef695dc9a03d2df4d236a3421
jessasesh <jessica.dison@yahoo.com>
2024-05-08 13:33:17 -0500

    Refactor budget function to use ES6 computed property names

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,14 +1,21 @@
+// Function to get the current year
 function getCurrentYear() {
+  // Create a new Date object to get the current date and time
   const date = new Date();
+  // Return the year using the getFullYear method
   return date.getFullYear();
 }
 
+// Export a function to get the budget for the current year
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
+  // Initialize an empty object to store the budget
+  const budget = {
+    // Use ES6 computed property names to dynamically generate property names
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita
+  };
 
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
-
+  // Return the budget object
   return budget;
 }
