c2c057512b897854175e7af473cd0b790ef5f74d
jessasesh <jessica.dison@yahoo.com>
2024-05-08 13:58:41 -0500

    Corrected concat syntax

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-  // Loop through each value in the array using for...of loop
-  for (let value of array) {
-    // Concatenate appendString to each value
-    value = appendString + value;
-  }
+  const updatedArray = [];
 
-  return array;
+  // Loop through each value in the array using for...of loop
+  for (const value of array) {
+    // Concatenate appendString to each value
+    updatedArray.push(appendString + value);
+  }
+  return updatedArray;
 }
