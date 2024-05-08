bd6221324914336b3437507eedf173ac9cdbb73f
jessasesh <jessica.dison@yahoo.com>
2024-05-08 13:46:01 -0500

    Updated syntax and added comments for better understanding

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,16 +1,27 @@
+// Importing the getBudgetObject function from an external module
 import getBudgetObject from './7-getBudgetObject.js';
 
+// Defining a function to get the full budget object
 export default function getFullBudgetObject(income, gdp, capita) {
+  // Getting the budget object using the imported function
   const budget = getBudgetObject(income, gdp, capita);
+  
+  // Creating the fullBudget object with additional methods
   const fullBudget = {
+    // Spread operator to include properties from the budget object
     ...budget,
+    
+    // Method to convert income to dollars
     getIncomeInDollars(income) {
       return `$${income}`;
     },
+    
+    // Method to append "euros" to income
     getIncomeInEuros(income) {
       return `${income} euros`;
     },
   };
 
+  // Returning the full budget object
   return fullBudget;
 }
