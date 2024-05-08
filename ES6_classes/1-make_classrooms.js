5ed4cd27603787f1d8fa9451a7fc93bf7da2b96c
jessasesh <jessica.dison@yahoo.com>
2024-05-08 14:44:15 -0500

    Created classrooms with resprective sizes

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,1 +1,10 @@
-initializeRooms
+import ClassRoom from './0-classroom';
+
+const initializeRooms = () => {
+  /* Stores values in a new array */
+  const classSize = [19, 20, 34];
+  /* Creates new instances for each size in array! */
+  return classSize.map((size) => new ClassRoom(size));
+};
+
+export default initializeRooms;
