a471380ddf2232fef43ad219ba79b5dbff0930ba
jessasesh <jessica.dison@yahoo.com>
2024-05-08 23:18:36 -0500

    Added comments

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,7 +1,14 @@
-//  function named guardrail that will accept one argument mathFunction
-// reutrn "queue"
-
-[
-  1000,
-  'Guardrail was processed',
-]
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let valueReturned;
+  try {
+    // Execute mathFunction and store result
+    valueReturned = mathFunction();
+  } catch (error) {
+    valueReturned = error.toString();
+  }
+  // Append value/message and return new array
+  queue.push(valueReturned);
+  queue.push('Guardrail was processed');
+  return queue;
+}
