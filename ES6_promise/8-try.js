d6d67a31b2a84fde40e54c72485451237efa26d3
jessasesh <jessica.dison@yahoo.com>
2024-05-08 23:11:37 -0500

    Using errors for math

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,3 +1,8 @@
+// Math machine
 export default function divideFunction(numerator, denominator) {
-
+  // Validator
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return (numerator / denominator);
 }
