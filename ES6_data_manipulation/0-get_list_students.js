97831c1c7b5574dc01b71725a8de10441d85140f
jessasesh <jessica.dison@yahoo.com>
2024-05-08 21:15:08 -0500

    Created func to return an array of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  // Return array of student objects
+  return [
+    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+  ];
+}
