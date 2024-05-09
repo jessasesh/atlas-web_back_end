3d080c3d069bef220f7822babb7ac881b42e8b25
jessasesh <jessica.dison@yahoo.com>
2024-05-08 21:02:32 -0500

    Changed line endings

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,1 +1,21 @@
-Car
+export default class Car {
+  // Constructor with attributes
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // Static getter for Symbol.species property
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  // Methid to clone car instance
+  cloneCar() {
+    // Retrieve the Species class
+    const Species = this.constructor[Symbol.species];
+    // Create new instance of Species class with same parameters
+    return new Species(this._brand, this._motor, this._color);
+  }
+}
