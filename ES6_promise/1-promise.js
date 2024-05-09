f1fab1a255b5ffabd6db58724399d7f865c898c6
jessasesh <jessica.dison@yahoo.com>
2024-05-08 22:42:49 -0500

    If, else promise

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,1 +1,15 @@
-getFullResponseFromAPI(success)
+export default function getFullResponseFromAPI(success) {
+  // Returns a promise
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      // If successful, resolve promise with success response object
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      // If unsuccessful, reject promise with error object
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
