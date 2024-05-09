871b3185ca257cbc9452481624cafe5d3ce37407
jessasesh <jessica.dison@yahoo.com>
2024-05-08 20:30:37 -0500

    Added typeerrors

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -1,1 +1,22 @@
-Airport
+// Define a class named Airport
+export default class Airport {
+  // Initialize Airport object with a name and a code
+  constructor(name, code) {
+    // String validator
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    // String validator
+    if (typeof code !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    // Assign name parameter to _name property
+    this._name = name;
+    // Assign code parameter to _code property
+    this._code = code;
+  }
+  // Method to return airport code
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
