8c2169854f4f16a00e7e323f512f5f79606551ec
jessasesh <jessica.dison@yahoo.com>
2024-05-08 20:04:17 -0500

    Added comments

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,1 +1,20 @@
-Building
+export default class Building {
+  constructor(sqft) {
+    // Check if the constructor is called directly from Building or from a subclass
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      // Throw an error if a subclass doesn't override evacuationWarningMessage method
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+    // Initialize the _sqft property with the provided value
+    this._sqft = sqft;
+  }
+  // Getter method for accessing the _sqft property
+  get sqft() {
+    return this._sqft;
+  }
+}
