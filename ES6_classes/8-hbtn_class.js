9d24ad10ddc4398ce7b258dc0eff2caea2600ad9
jessasesh <jessica.dison@yahoo.com>
2024-05-08 20:42:46 -0500

    Fixed validators

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,1 +1,25 @@
-HolbertonClass
+export default class HolbertonClass {
+  // Constructor with attributes
+  constructor(size, location) {
+    // Number validator
+    if (typeof size !== 'number') {
+      throw TypeError('Size must be a number');
+    }
+    // String validator
+    if (typeof location !== 'string') {
+      throw TypeError('Location must be a string');
+    }
+    // Asssign size parameter to _size property
+    this._size = size;
+    // Assign location parameter to _location property
+    this._location = location;
+  }
+  // Return size value of object 
+  retrieveSize() {
+    return this._size;
+  }
+  // Return location string of object
+  retrieveLocation() {
+    return this._location;
+  }
+}
