ef33c1886c2dd724b2b478678c716bc08861ac27
jessasesh <jessica.dison@yahoo.com>
2024-05-08 20:13:26 -0500

    Implemented return with warning message for specified floor

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,1 +1,27 @@
-SkyHighBuilding
+import Building from './5-building';
+// Define a new class named SkyHighBuilding that extends the Building class
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    // Check if the floors parameter is not a number
+    if (typeof floors !== 'number') {
+      // Throw a TypeError if floors is not a number
+      throw TypeError('Floors must be a number');
+    }
+    // Call the constructor of the superclass (Building) with the sqft parameter
+    super(sqft);
+    // Initialize the _floors property with the floors parameter
+    this._floors = floors;
+  }
+  // Getter to retrie value of _sqft property
+  get sqft() {
+    return this._sqft;
+  }
+  // Getter to retrie value of _flooor property
+  get floors() {
+    return this._floors;
+  }
+  // Warning message for specified floor
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
