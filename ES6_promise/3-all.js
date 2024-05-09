4ce82c3820e685a568a3192e27801e10c9686bad
jessasesh <jessica.dison@yahoo.com>
2024-05-08 22:56:29 -0500

    Implemented error message if either user promise fails

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,1 +1,17 @@
-function handleProfileSignup()
+import { uploadPhoto, createUser } from './utils';
+
+// Asynch handling for profile signup process
+export default function handleProfileSignup() {
+  // Concurrently execute the uploadPhoto() and createUser() functions
+  return Promise.all([uploadPhoto(), createUser()])
+    // Breakdown response objects
+    .then((response) => {
+      // Extract the body from photo upload response
+      const { body } = response[0];
+      // Extract first/last names from user response
+      const { firstName, lastName } = response[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    // Log error message if any promise rejects
+    .catch(() => console.log('Signup system offline'));
+}
