e065104ebbf73aa1f224c8030bc34491e5b38145
jessasesh <jessica.dison@yahoo.com>
2024-05-08 22:43:47 -0500

    Asynch type handling for promise

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,1 +1,14 @@
-function handleResponseFromAPI(promise)
+export default function handleResponseFromAPI(promise) {
+  // Function takes promise as input and returns new promise
+  return promise
+    // Asynch handling for API response
+    // Resolve with success response object
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    // Reject with empty error object
+    .catch(() => Error())
+    // Regardless, log API response message
+    .finally(() => console.warn('Got a response from the API'));
+}
