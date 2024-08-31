d36603a80a02d0fe9ea20627e84c85cebe13f6ca
jessasesh <jessica.dison@yahoo.com>
2024-08-31 11:03:57 -0500

    Func parameters and doc

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 """
-Function which takes a float as argument and returns the floor of the float.
+Function which takes a float as argument
+and returns the floor of the float.
 """
 
-def floor():
-    return floor
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns floor of a float.
+
+    Args:
+        n: number to floor.
+
+    Returns:
+        int: floor of the float
+    """
+    return math.floor(n)
