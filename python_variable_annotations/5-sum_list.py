60fa0e918a29ce9732a7b77a8ef6a0f872ab1337
jessasesh <jessica.dison@yahoo.com>
2024-08-31 13:31:29 -0500

    Defined params, used type hint, and added doc

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 """
-Function which takes a lis of floats as argument and returns their sum as a float.
+Function which takes a list of floats as argument
+and returns their sum as a float.
 """
 
-def sum_list():
-    return sum(float)
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Sum of lists.
+
+    Args:
+        input_list: list of floats to add.
+
+    Returns:
+        float: sum of floats in the list.
+    """
+    return sum(input_list)
