15c127cc99fbe5660b748ed6f948901c3537f49f
jessasesh <jessica.dison@yahoo.com>
2024-08-31 14:55:05 -0500

    Async coroutine with delay

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+"""
+Asynchronous coroutine that takes in an
+integer argument that waits for a random
+delay and eventually returns it.
+"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Async coroutine with delay.
+
+    Args:
+        max_delay: maximum delay in seconds.
+
+    Returns:
+        float: delay time in seconds.
+    """
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
