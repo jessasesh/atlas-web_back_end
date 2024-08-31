acd05572260ee0d59334c7bdb264990b99560767
jessasesh <jessica.dison@yahoo.com>
2024-08-31 10:53:30 -0500

    Syntax error fix

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,8 +4,12 @@ Function that takes two strings as arguments and returns a concatenated string.
 """
 
 
-def concat(str1: str. str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """
     Concatenates and returns the result.
+
+    Args:
+        str1: first string
+        str2: second string
     """
     return str1 + str2
