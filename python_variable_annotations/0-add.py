324e471307b49f3db3bac74d8681d09dcd5b3b2d
jessasesh <jessica.dison@yahoo.com>
2024-08-31 10:34:10 -0500

    Fixed pycodestyle error

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Function that takes two float values as arguments and returns their sum as a float.
+Function that takes two float values as arguments
+and returns their sum as a float.
 """
 
 def add(a: float, b: float) -> float:
