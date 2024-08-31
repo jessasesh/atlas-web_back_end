e3472b136a1a5a23bd98133f10b79a195c307c03
jessasesh <jessica.dison@yahoo.com>
2024-08-31 10:32:59 -0500

    Updated func with required params and doc

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,5 +3,8 @@
 Function that takes two float values as arguments and returns their sum as a float.
 """
 
-def add():
+def add(a: float, b: float) -> float:
+    """
+    Adds two floats and returns the sum.
+    """
     return a + b
