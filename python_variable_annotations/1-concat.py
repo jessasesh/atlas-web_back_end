20ddc2b8acede4ed2c3adf5b15b35ee3029d54d2
jessasesh <jessica.dison@yahoo.com>
2024-08-31 10:47:06 -0500

    Updated func with required params and doc

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,5 +3,9 @@
 Function that takes two strings as arguments and returns a concatenated string.
 """
 
-def concat():
+
+def concat(str1: str. str2: str) -> str:
+    """
+    Concatenates and returns the result.
+    """
     return str1 + str2
