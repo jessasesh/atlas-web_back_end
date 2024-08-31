9e2ef70c43f6e3e8633233ab30fbb6c6cc923cfe
jessasesh <jessica.dison@yahoo.com>
2024-08-31 10:40:27 -0500

    Installed pycodestyle and fixed error

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,6 +4,7 @@ Function that takes two float values as arguments
 and returns their sum as a float.
 """
 
+
 def add(a: float, b: float) -> float:
     """
     Adds two floats and returns the sum.
