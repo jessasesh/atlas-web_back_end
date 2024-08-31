1c12e86680935d55335e4f52fc72de98c22c5347
jessasesh <jessica.dison@yahoo.com>
2024-08-31 13:51:11 -0500

    Defined args NOT params -_-
    Added imports and req doc

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,5 +3,19 @@
 Function that creates tuples from a string and a number.
 """
 
-def to_kv():
-    return tuple
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Tuple that has str for first element and second element
+    is the square of the float.
+
+    Args:
+        k: str value
+        v: int value
+
+    Returns:
+        a tuple
+    """
+    return (k, float(v ** 2))
