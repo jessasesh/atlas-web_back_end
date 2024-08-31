d36f413cc6a2351fbcc45fa3d9bcf2b3cb35c28a
jessasesh <jessica.dison@yahoo.com>
2024-08-31 11:15:18 -0500

    Updated func with params, type hint, and doc

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 """
-Function that takes a float as argument and returns the string representation of the float.
+Function that takes a float as argument and returns the string
+representation of the float.
 """
 
-def to_str():
-    return str
+
+def to_str(n: float) -> str:
+    """
+    Converts float into string representation.
+
+    Args:
+        n: float number to convert.
+
+    Returns:
+        str: string representation of float number.
+    """
+    return str(n)
