655ce2d52dcd93836d6d9aaad42ca3a3a15ea43a
jessasesh <jessica.dison@yahoo.com>
2024-08-31 14:30:25 -0500

    Function that returns a list of tuples

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 """
-
+Fucntion that returns a list of tuples.
 """
 
-def element_length(lst):
+from typing import List, Tuple, Sequence
+
+
+def element_length(lst: List[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    List of tuples that each contain an element
+    of the input list and its length.
+
+    Args:
+        lst: list of sequences.
+
+    Returns:
+        List: a list of tuples where each one contains a
+        sequence and its length.
+    """
     return [(i, len(i)) for i in lst]
