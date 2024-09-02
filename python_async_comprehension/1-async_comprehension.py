5c28c2c3ff064152017c0c59b3d9c4a2c29400bd
jessasesh <jessica.dison@yahoo.com>
2024-09-01 23:19:59 -0500

    Imported number generator and implemented async comprehension to collect numbers; tested locally

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""
+Coroutine that collects 10 random numbers from async_generator.
+"""
+
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Coroutine that collects 10 random numbers from async_generator.
+
+    Returns: list of 10 random numbers
+    """
+    return [number async for number in async_generator()]
