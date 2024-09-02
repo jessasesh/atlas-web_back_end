4986a3fd258182773bf8b4c7f004c332e2875449
jessasesh <jessica.dison@yahoo.com>
2024-09-01 22:19:01 -0500

    Reused code from task 1; updated func name; tested locally

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,1 +1,31 @@
 #!/usr/bin/env python3
+"""
+Async routine that takes in two
+int arguments in specific order.
+"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__("3-tasks").task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronously spawn task_wait_random, n times with
+    specified max_delay.
+
+    Args:
+        n: number of times to spawn wait_random
+        max_delay: max delay for wait_random
+
+    Returns: List of all delays in ascending order.
+    """
+    delay_list: List[float] = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    for completed_task in asyncio.as_completed(tasks):
+        delay = await completed_task
+        delay_list.append(delay)
+
+    return delay_list
