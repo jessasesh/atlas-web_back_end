35eeecfd58dad9698d7b7f99025a18e82285ca0b
jessasesh <jessica.dison@yahoo.com>
2024-09-01 23:40:16 -0500

    Measure runtime in parallel; confirmed runtime is 10 seconds and not 40; tested locally

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+"""
+Coroutine to measure runtime with parallel execution.
+"""
+
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Measure total runtime.
+
+    Returns: total runtime in seconds.
+    """
+    beginning_of_time = time.perf_counter()
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    end_of_time = time.perf_counter()
+    total_runtime = end_of_time - beginning_of_time
+
+    return total_runtime
