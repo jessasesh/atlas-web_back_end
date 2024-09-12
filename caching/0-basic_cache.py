d6793995a931065805bfc33ace63f80e7f69a510
jessasesh <jessica.dison@yahoo.com>
2024-09-11 22:58:41 -0500

    Defined put and get functions; tested locally

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+
+    def put(self, key, item):
+        """
+        Assigns dictionary to item value,
+        if key or item is none it does nothing.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Returns the value in dictionary to linked
+        key, if key is none or doesnt exist it
+        returns nothing.
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
