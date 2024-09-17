f1d27d3ef40e4145a1210f0f4d4846f774579f9d
jessasesh <jessica.dison@yahoo.com>
2024-09-16 23:26:40 -0500

    Function that returns start/end indexes; tested locally

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+Simple Helper Function
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Calculates the start and end index for pagination.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
