e444ef4efd3ecf798106c688955d18d92a9856c5
jessasesh <jessica.dison@yahoo.com>
2024-09-17 21:33:33 -0500

    Implement pagination and output formatting for dataset rows; tested locally

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,1 +1,51 @@
 #!/usr/bin/env python3
+"""
+Simple Pagination
+"""
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Calculates the start and end index for pagination.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            """
+            Retrieves a page from the given dataset.
+            """
+            if not isinstance(page, int) or not isinstance(page_size, int):
+                 return []
+            if page <= 0 or page_size <= 0:
+                 return[]
+            
+            dataset = self.dataset()
+            start_index, end_index = index_range(page, page_size)
+            return dataset[start_index:end_index] if start_index < len(dataset) else []
