62430f95e5916c3106ae99ba4015d9a0b3dcdf86
jessasesh <jessica.dison@yahoo.com>
2024-09-22 15:49:18 -0500

    Defined regex filter to obfusctate data log

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,1 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/python 3
+"""
+Function that returns obfuscacted log message
+"""
+import re
+from typing import List
+
+
+def filter_datum(
+    fields: List[str],
+    redaction: str,
+    message: str,
+    separator: str
+) -> str:
+    """
+    Returns obfuscated data
+    """
+    escaped_fields = [re.escape(field) for field in fields]
+    filter = r'(' + '|'.join(escaped_fields) + r')=([^;{}]+)'.format(separator)
+
+    return re.sub(filter, r'\1=' + redaction, message)
