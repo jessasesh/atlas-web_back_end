9766c98d008be14018d6d59a83772ac36595e3a1
jessasesh <jessica.dison@yahoo.com>
2024-09-23 15:02:44 -0500

    Create user with auth

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -10,9 +10,25 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def welcome():
     """
-    GET route that returns a JSON payload with a welcome message.
+    GET method with message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """
+    Register User
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        AUTH._db.find_user_by(email=email)
+        return jsonify({"message": "email already registered"}), 400
+    except NoResultFound:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
 
 
 if __name__ == "__main__":
