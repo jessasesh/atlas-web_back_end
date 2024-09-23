58c07dddb69d987ccf900e52680bc7c7cca49725
jessasesh <jessica.dison@yahoo.com>
2024-09-22 19:26:35 -0500

    Implemented function to validate password against hashed password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -13,3 +13,11 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that password matches the hashed password.
+
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
