90415891cc72be131281365528fbd3f0c018a5d6
jessasesh <jessica.dison@yahoo.com>
2024-09-23 14:55:49 -0500

    Basic Flask app

---
+++ user_authentication_service/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Flask App
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def welcome():
+    """
+    GET route that returns a JSON payload with a welcome message.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
