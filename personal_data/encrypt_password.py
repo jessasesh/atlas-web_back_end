14dfa5cbdf0003821be01ea11fcee1449152adbf
jessasesh <jessica.dison@yahoo.com>
2024-09-22 19:24:26 -0500

    Fix for PEP error

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -4,11 +4,12 @@ Encrypting passwords
 """
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """
     Hashes a password using bcrypt
     """
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-    
+
     return hashed
