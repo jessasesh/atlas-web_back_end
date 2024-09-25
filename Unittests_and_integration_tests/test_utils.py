e1a83c423fba21cc4dff46d6c064758de25085d5
jessasesh <jessica.dison@yahoo.com>
2024-09-25 17:30:34 -0500

    Parameterized test for accessing nested map

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Unit Test for utils functions
+"""
+
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected) -> None:
+        self.assertEqual(access_nested_map(nested_map, path), expected)
