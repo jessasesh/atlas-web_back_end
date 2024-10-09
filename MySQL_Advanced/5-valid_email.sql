e84eb2d6e10d2b5c3af628cf9348caee08068162
jessasesh <jessica.dison@yahoo.com>
2024-10-09 16:48:53 -0500

    Trigger to set validity for new emails after being changed

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+-- Trigger that resets valid_email when the email has been changed
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
