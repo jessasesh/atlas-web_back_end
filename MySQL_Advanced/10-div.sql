7f0e548c4469966a950dcaaf358e1c44310a5447
jessasesh <jessica.dison@yahoo.com>
2024-10-09 19:32:16 -0500

    Create function to safely divide two numbers, returning 0 when the denominator is 0 to avoid undefined error

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,17 +1,13 @@
--- Create a function SafeDiv to divide two numbers or return 0 if the second number is 0
-
+-- Create function to divide two numbers
 DELIMITER $$
 
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10,2)
+RETURNS DECIMAL
 DETERMINISTIC
 BEGIN
-    -- Check if the second argument (b) is zero
     IF b = 0 THEN
-        -- Return 0 if b is zero
         RETURN 0;
     ELSE
-        -- Return the result of a divided by b
         RETURN a / b;
     END IF;
 END$$
